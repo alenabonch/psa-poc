1d520fda81231fe23648173f0a499cff6542b7b3
Elena Vinokurova <Elena_Vinokurova@epam.com>
2017-11-10 17:26:47 +0300

    sec

--- App.js
+++ App.js
@@ -1,98 +1,82 @@
 import React, {Component} from 'react';
 import { View, Text, TouchableOpacity, AppRegistry, StyleSheet, ToolbarAndroid } from 'react-native';
-import { Camera, Permissions } from 'expo';
+import Camera from 'react-native-camera';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import BottomNavigation, { Tab } from 'react-native-material-bottom-navigation'
 
 export default class Bananas extends Component {
-    state = {
-        hasCameraPermission: null,
-        type: Camera.Constants.Type.back,
-    };
-
-    async componentWillMount() {
-        const { status } = await Permissions.askAsync(Permissions.CAMERA);
-        this.setState({ hasCameraPermission: status === 'granted' });
-    }
+    // state = {
+    //     hasCameraPermission: null,
+    //     type: Camera.Constants.Type.back,
+    // };
+    //
+    // async componentWillMount() {
+    //     const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    //     this.setState({ hasCameraPermission: status === 'granted' });
+    // }
 
     action() {
-        //code
+        //code..
+    }
+
+    takePicture() {
+        const options = {};
+        //options.location = ...
+        this.camera.capture({metadata: options})
+            .then((data) => console.log(data))
+            .catch(err => console.error(err));
     }
 
     render() {
-        const {hasCameraPermission} = this.state;
-        if (hasCameraPermission === null) {
-            return <View/>;
-        } else if (hasCameraPermission === false) {
-            return <Text>No access to camera</Text>;
-        } else {
-            return (
-                <View style={{flex: 1}}>
-                    <ToolbarAndroid style={styles.toolbar}>
-                        <TouchableOpacity onPress={this.action.bind(this)}>
-                            <Icon name="menu" size={26} color="white" style={styles.menuButton} />
-                        </TouchableOpacity>
-                    </ToolbarAndroid>
+        return (
+            <View style={{flex: 1}}>
+                <ToolbarAndroid style={styles.toolbar}>
+                    <TouchableOpacity onPress={this.action.bind(this)}>
+                        <Icon name="menu" size={26} color="white" style={styles.menuButton} />
+                    </TouchableOpacity>
+                </ToolbarAndroid>
 
-                    <Camera style={{flex: 1}} type={this.state.type}>
-                        <View
-                            style={{
-                                flex: 1,
-                                backgroundColor: 'transparent',
-                                flexDirection: 'row'
-                            }}>
-                            <TouchableOpacity
-                                style={{
-                                    flex: 0.1,
-                                    alignSelf: 'flex-end',
-                                    alignItems: 'center'
-                                }}
-                                onPress={() => {
-                                    this.setState({
-                                        type: this.state.type === Camera.Constants.Type.back
-                                            ? Camera.Constants.Type.front
-                                            : Camera.Constants.Type.back
-                                    });
-                                }}>
-                                <Text
-                                    style={{fontSize: 18, marginBottom: 10, color: 'white'}}>
-                                    {' '}Flip!!{' '}
-                                </Text>
-                            </TouchableOpacity>
-                        </View>
+                <View style={styles.container}>
+                    <Camera
+                        ref={(cam) => {
+                            this.camera = cam;
+                        }}
+                        style={styles.preview}
+                        aspect={Camera.constants.Aspect.fill}>
+                        <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
                     </Camera>
-
-                    <BottomNavigation
-                        labelColor="white"
-                        rippleColor="white"
-                        activeTab={1}
-                        style={{ height: 56, elevation: 8, position: 'absolute', left: 0, bottom: 0, right: 0 }}
-                        onTabChange={(newTabIndex) => alert(`New Tab at position ${newTabIndex}`)}
-                    >
-                        <Tab
-                            barBackgroundColor="#37474F"
-                            label="Video"
-                            icon={<Icon size={24} color="white" name="switch-video" />}
-                        />
-                        <Tab
-                            barBackgroundColor="#00796B"
-                            label="Photo"
-                            icon={<Icon size={24} color="white" name="camera-alt" />}
-                        />
-                        <Tab
-                            barBackgroundColor="#5D4037"
-                            label="Audio"
-                            icon={<Icon size={24} color="white" name="keyboard-voice" />}
-                        />
-                        <Tab
-                            barBackgroundColor="#3E2723"
-                            label="Note"
-                            icon={<Icon size={24} color="white" name="insert-comment" />}
-                        />
-                    </BottomNavigation>
                 </View>
-            );
-        }
+
+                <BottomNavigation
+                    labelColor="white"
+                    rippleColor="white"
+                    activeTab={1}
+                    style={{ height: 56, elevation: 8, position: 'absolute', left: 0, bottom: 0, right: 0 }}
+                    onTabChange={(newTabIndex) => alert(`New Tab at position ${newTabIndex}`)}
+                >
+                    <Tab
+                        barBackgroundColor="#37474F"
+                        label="Video"
+                        icon={<Icon size={24} color="white" name="switch-video" />}
+                    />
+                    <Tab
+                        barBackgroundColor="#00796B"
+                        label="Photo"
+                        icon={<Icon size={24} color="white" name="camera-alt" />}
+                    />
+                    <Tab
+                        barBackgroundColor="#5D4037"
+                        label="Audio"
+                        icon={<Icon size={24} color="white" name="keyboard-voice" />}
+                    />
+                    <Tab
+                        barBackgroundColor="#3E2723"
+                        label="Note"
+                        icon={<Icon size={24} color="white" name="insert-comment" />}
+                    />
+                </BottomNavigation>
+            </View>
+        );
     }
 }
 
@@ -103,6 +87,23 @@ const styles = StyleSheet.create({
     },
     menuButton: {
         marginTop: 10
+    },
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+    },
+    preview: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center'
+    },
+    capture: {
+        flex: 0,
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        color: '#000',
+        padding: 10,
+        margin: 40
     }
 });
 
